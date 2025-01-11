093330f2d21ca87e04a07e0fc932e169f60491f8
Kris Hvattum <klhvattum@live.com>
2025-01-10 23:42:37 -0800

    remove hard coded allowed hosts

--- spellrite_project/spellrite_project/settings.py
+++ spellrite_project/spellrite_project/settings.py
@@ -29,8 +29,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
 
-# ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '137.184.82.233']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+
 
 DATABASES = {
     'default': {
